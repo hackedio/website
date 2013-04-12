2942caf2a558e3a83dcf2b84e7df81db9d8f4877
Robert Lee-Cann <robert.lee-cann@stylefoundry.co.uk>
2013-04-12 16:56:21 +0100

    Get blog working. Switch to friendly URLs

--- config.rb
+++ config.rb
@@ -19,19 +19,19 @@ end
 activate :blog do |blog|
   blog.prefix = "blog"
 
-  # blog.permalink = ":year/:month/:day/:title.html"
+  blog.permalink = ":year/:month/:day/:title.html"
   # blog.sources = ":year-:month-:day-:title.html"
   # blog.taglink = "tags/:tag.html"
   # blog.layout = "layout"
   # blog.summary_separator = /(READMORE)/
   # blog.summary_length = 250
-  # blog.year_link = ":year.html"
-  # blog.month_link = ":year/:month.html"
-  # blog.day_link = ":year/:month/:day.html"
-  # blog.default_extension = ".markdown"
+  blog.year_link = ":year.html"
+  blog.month_link = ":year/:month.html"
+  blog.day_link = ":year/:month/:day.html"
+  blog.default_extension = ".erb"
 
-  blog.tag_template = "tag.html"
-  blog.calendar_template = "calendar.html"
+  #blog.tag_template = "blog/tag.html"
+  blog.calendar_template = "blog/calendar.html"
 
   # blog.paginate = true
   # blog.per_page = 10
@@ -41,6 +41,10 @@ end
 page "/feed.xml", :layout => false
 
 activate :livereload, grace_period: 0.5
+
+activate :directory_indexes
+
+page "404.html", directory_index: false
 
 ###
 # Page command
