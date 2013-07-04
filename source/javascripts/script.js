9162ffc50c71467ff1545155c41ccded9a26dee7
SeanHolden <sean.holden@o2.com>
2013-07-04 15:09:01 +0100

    added get-help form and some simple JS validations to ensure fields are filled in.

--- source/javascripts/script.js
+++ source/javascripts/script.js
@@ -2,25 +2,39 @@
 
 */
 
+$(function(){
+  initGetHelpForm();
+});
 
+// JS for /almanac/get-help
 
+function initGetHelpForm(){
+  path = window.location.pathname;
+  if(path.match('\/almanac\/get-help\/?')){
+    validateHelpForm();
+  };
+}
 
+function validateHelpForm(){
+  $('#helpticket').submit(function(){
+    if (helpFormIsValid()){
+      return true;
+    }else{
+      alert('Please ensure all fields are filled in');
+      return false;
+    };
+  });
+}
 
+function helpFormIsValid(){
+  var name = $('#name').val();
+  var seat = $('#seat').val();
+  var desc = $('#desc').val();
+  if (name == "" || seat == "" || desc == ""){
+    return false;
+  }else{
+    return true
+  };
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// End of JS for /almanac/get-help
