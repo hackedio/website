48bbc035a4c79b6de11dccb515bfb42f7d3da918
Weebo <admin@o2labs.co.uk>
2013-07-19 14:44:13 +0000

    Automated commit at 2013-07-19 14:44:13 UTC by middleman-deploy 3.0.13

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,4 +1,4 @@
 /* Author: 
 
 */
-;
+function initGetHelpForm(){path=window.location.pathname,path.match("/almanac/get-help/?")&&validateHelpForm()}function validateHelpForm(){$("#helpticket").submit(function(){return helpFormIsValid()?!0:(alert("Please ensure all fields are filled in"),!1)})}function helpFormIsValid(){var e=$("#name").val(),t=$("#seat").val(),n=$("#desc").val();return e==""||t==""||n==""?!1:!0}$(function(){initGetHelpForm()});
